5f804f481b43b8704bdd8f5065f535e9179ff40a
Derrick Lannaman <derricklannaman@gmail.com>
2012-12-30 21:07:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LannamanPortfolio::Application.config.secret_token = '539781164e3e31cd809a79e295aa99810592e2dc0d26481cb8857e9c236078c9caedb900fcb90815c1c4b49306a9b53c81ddf80a894b66369f336c62ecffe333'
