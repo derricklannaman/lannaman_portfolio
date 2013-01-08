aac39826472eabcd6a1dc275864f23143f9d4ba4
Derrick Lannaman <derricklannaman@gmail.com>
2013-01-07 19:54:30 -0500

    add lightbox to app slide

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,8 @@ class HomeController < ApplicationController
   quotes = []
   quotes << "Yeah, I'm comfortable with " << "I'm constantly learning new things in " << "I Love " << "Because hard work pays off, I know " << "I dream in " << "Practice only makes for improvement. So, I practice " << "The road to full-stack enlightenment is paved with "
   @quote = quotes.sort.sample
+
+  @foodie_url = "http://www.shrouded-ravine-1795.herokuapp.com/"
 	end
 
 end
