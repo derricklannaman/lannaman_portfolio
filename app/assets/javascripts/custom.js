c7fc1424c336c49d1bfbc62e8522126b9e37c62c
Derrick Lannaman <derricklannaman@gmail.com>
2013-01-27 17:40:19 -0500

    improve formatting

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,5 @@
 $(function(){
-
+// page intro
  $('#top-nav').hide();
  $('#slide1').hide();
  $('#slide2').hide();
@@ -7,7 +7,7 @@ $(function(){
  $('#slide4').hide();
  $('#slide5').hide();
 
-
+// hides content panels
  $('#full-stack').hide();
  $('#down-arrow-1').hide();
  $('#down-arrow-2').hide();
@@ -18,12 +18,16 @@ $(function(){
  $('.fourth-slide-content').hide();
  $('.light-box-caption').hide();
 
+// get results button for full stack baby
  $('#result-btn').click(show_stack);
 
+// click on questions to reveal full content panel
  $('#question-1').click(show_slide1);
  $('#question-2').click(show_slide2);
  $('#question-3').click(show_slide3);
  $('#question-4').click(show_slide4);
+
+ // in projects, hoving over image will reveal caption
  $('#beautiflow-light-box').hover(show_beautiflow_caption, hide_beautiflow_caption);
  $('#foodie-light-box').hover(show_foodie_caption, hide_foodie_caption);
  $('#susunami-light-box').hover(show_susunami_caption, hide_susunami_caption);
@@ -32,7 +36,7 @@ $(function(){
 
 // Onload fade in intro of page
 $(function() {
-  $('#top-nav').fadeIn(1000);
+  $('#top-nav').fadeIn(200);
   $('#slide1').delay(300).fadeIn(1200);
   $('#slide2').delay(600).fadeIn(1200);
   $('#slide3').delay(900).fadeIn(1200);
@@ -41,6 +45,7 @@ $(function() {
 });
 
 
+// Projects panel => reveals captions when hovering over images
 function show_susunami_caption(){
   $('#susunami-caption').slideDown(500);
 }
@@ -62,14 +67,15 @@ function hide_beautiflow_caption(){
   $('#beautiflow-caption').slideUp(500);
 }
 
-
-
+// "Projects" panel => reveals full content panel
 function show_slide4(){
  $('#slide4').css({
       height:   '100%'
     });
  $('.fourth-slide-content').fadeIn(1500);
 }
+
+// "What do you know?" panel => reveals full content panel
 
 function show_slide3(){
  $('#down-arrow-3').fadeIn(1000);
@@ -81,6 +87,13 @@ function show_slide3(){
 
 }
 
+// "What do you know?" panel => remove do block & fades in skill set quotes
+function show_stack(){
+  $('#full-stack').fadeIn(700);
+  $('#do-block').remove();
+}
+
+// "Nice fella" panel => reveals full content panel
 function show_slide2(){
  $('#down-arrow-2').fadeIn(1000);
 
@@ -91,8 +104,7 @@ function show_slide2(){
 
 }
 
-
-
+// "Who am I?" panel => reveals full content panel
 function show_slide1(){
  $('#down-arrow-1').fadeIn(1000);
 
@@ -104,10 +116,6 @@ function show_slide1(){
 
 }
 
-function show_stack(){
-  $('#full-stack').fadeIn(700);
-  $('#do-block').remove();
-}
 
 
 
